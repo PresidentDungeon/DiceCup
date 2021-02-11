81893f6df191c332fd35c3e06f61fd0bf39de1c8
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-02-11 12:18:40 +0100

    Added spinner with da bois

--- DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
+++ DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.easv.aepm.dicecup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import androidx.core.view.children
-import androidx.core.view.size
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     val mGenerator = Random()
-    val diceIds = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
+    val diceIds = arrayOf(
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6
+    )
     val history = mutableListOf<Pair<Int, Int>>()
 
 
@@ -22,38 +29,72 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(savedInstanceState != null){
-            val history = savedInstanceState.getSerializable("history") as Array<Pair<Int, Int>>
+        selectSpinner.setOnItemSelectedListener(object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
 
-            history.forEach { p -> this.history.add(p) }
-            updateHistory()
-            if(history.size > 0){updateDicesWith(history[history.size-1])}
-        }
+                diceBoard.removeAllViews()
+
+
+                //btnRoll.visibility = View.VISIBLE
+
+                if(position != 0){
+                    generateDice(position)
+                }
+                else{
+                    //hide roll and clear
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        })
+
+        if(savedInstanceState != null){ }
     }
 
-    fun onClickRoll(view: View) {
+    fun generateDice(amount: Int){
 
-        val d1 = mGenerator.nextInt(6)
-        val d2 = mGenerator.nextInt(6)
 
-        imgDice1.setImageResource(diceIds[d1])
-        imgDice2.setImageResource(diceIds[d2])
+        for (i in 1..amount){
+            val horizontalLayout = LinearLayout(this)
+            horizontalLayout.orientation = LinearLayout.HORIZONTAL
 
-        if(history.size >= 5){
-            history.removeAt(0)
+
+            val btn = ImageButton(this)
+            btn.setImageResource(this.diceIds[0])
+            diceBoard.addView(btn)
+
         }
 
-        history.add(Pair(d1 + 1, d2 + 1))
 
-        updateHistory()
+
 
     }
+
+
+
+
+
+//    fun onClickRoll(view: View) {
+//
+//        val d1 = mGenerator.nextInt(6)
+//        val d2 = mGenerator.nextInt(6)
+//
+//        imgDice1.setImageResource(diceIds[d1])
+//        imgDice2.setImageResource(diceIds[d2])
+//
+//        if(history.size >= 5){
+//            history.removeAt(0)
+//        }
+//
+//        history.add(Pair(d1 + 1, d2 + 1))
+//
+//        updateHistory()
+//
+//    }
 
     private fun updateHistory() {
 
         var historyString = ""
-        history.forEach{
-                p -> val d1 = p.first; val d2 = p.second
+        history.forEach{ p -> val d1 = p.first; val d2 = p.second
                 historyString += "$d1 - $d2\n"
         }
 
@@ -65,14 +106,14 @@ class MainActivity : AppCompatActivity() {
         updateHistory()
     }
 
-    private fun updateDicesWith(p: Pair<Int, Int>) {
-        imgDice1.setImageResource( diceIds[p.first-1] )
-        imgDice2.setImageResource( diceIds[p.second-1] )
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putSerializable("history", history.toTypedArray())
-    }
+//    private fun updateDicesWith(p: Pair<Int, Int>) {
+//        imgDice1.setImageResource( diceIds[p.first-1] )
+//        imgDice2.setImageResource( diceIds[p.second-1] )
+//    }
+//
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putSerializable("history", history.toTypedArray())
+//    }
 
 }
