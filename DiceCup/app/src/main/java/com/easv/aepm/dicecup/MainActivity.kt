394c0ea8eb193f9907ea3b6acae5bff9c7dbaff9
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-02-12 01:10:36 +0100

    Yatzi?

--- DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
+++ DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.easv.aepm.dicecup
 
+import android.graphics.Color
 import android.media.Image
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import androidx.core.view.setPadding
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.ceil
@@ -16,15 +18,12 @@ import kotlin.math.ceil
 class MainActivity : AppCompatActivity() {
 
     val mGenerator = Random()
-    val diceIds = arrayOf(
-        R.drawable.dice1,
-        R.drawable.dice2,
-        R.drawable.dice3,
-        R.drawable.dice4,
-        R.drawable.dice5,
-        R.drawable.dice6
-    )
-    val history = mutableListOf<Pair<Int, Int>>()
+    val diceIds = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
+    val diceIdsSelected = arrayOf(R.drawable.dice1s, R.drawable.dice2s, R.drawable.dice3s, R.drawable.dice4s, R.drawable.dice5s, R.drawable.dice6s)
+    val history = mutableListOf<Array<Int>>()
+
+    private lateinit var shouldRoll: Array<Boolean>
+    private var diceAmount: Int = 0
 
 
 
@@ -35,15 +34,19 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
 
                 diceBoard.removeAllViews()
-
-
-                //btnRoll.visibility = View.VISIBLE
+                diceAmount = position;
+                shouldRoll = Array(diceAmount){i -> true}
 
                 if(position != 0){
                     generateDice(position)
+                    btnRoll.visibility = View.VISIBLE
+                    tvHistory.visibility = View.VISIBLE
+                    btnClear.visibility = View.VISIBLE
                 }
                 else{
-                    //hide roll and clear
+                    btnRoll.visibility = View.INVISIBLE
+                    tvHistory.visibility = View.INVISIBLE
+                    btnClear.visibility = View.INVISIBLE
                 }
             }
             override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -94,37 +97,49 @@ class MainActivity : AppCompatActivity() {
 
     fun onClickImage(view: View){
 
-        Log.d("pikkemand", "${getIndexOfChild(view as ImageButton)}")
-
+        val imageButton: ImageButton = view as ImageButton
+        val placement:Int = getIndexOfChild(imageButton)
+        val historyLast: Int = if(history.size > 0) history[history.size-1][placement] - 1 else 0
+        shouldRoll[placement] = !shouldRoll[placement]
+        imageButton.setImageResource(if(!shouldRoll[placement]) diceIdsSelected[historyLast] else diceIds[historyLast])
     }
 
+    fun onClickRoll(view: View) {
 
+        val currentHistory: Array<Int> = Array<Int>(diceAmount){i -> 0}
+        val allButtons: Array<ImageButton> = getAllButtons()
 
-//    fun onClickRoll(view: View) {
-//
-//        val d1 = mGenerator.nextInt(6)
-//        val d2 = mGenerator.nextInt(6)
-//
-//        imgDice1.setImageResource(diceIds[d1])
-//        imgDice2.setImageResource(diceIds[d2])
-//
-//        if(history.size >= 5){
-//            history.removeAt(0)
-//        }
-//
-//        history.add(Pair(d1 + 1, d2 + 1))
-//
-//        updateHistory()
-//
-//    }
-
-    private fun updateHistory() {
-
-        var historyString = ""
-        history.forEach{ p -> val d1 = p.first; val d2 = p.second
-                historyString += "$d1 - $d2\n"
+        if(!shouldRoll.contains(false)){
+            for(i in allButtons.indices){
+                val number: Int = mGenerator.nextInt(6)
+                currentHistory[i] = number + 1
+                allButtons[i].setImageResource(diceIds[number])
+            }
+            history.add(currentHistory)
         }
 
+        else{
+            for(i in allButtons.indices) {
+                val number:Int = if(shouldRoll[i] === false) mGenerator.nextInt(6) else -1
+                if(number == -1 && history.size > 0){currentHistory[i] = history[history.size-1][i]}
+                else if(number == -1){currentHistory[i] = 0}
+                else{currentHistory[i] = number + 1}
+
+                if(number != -1){allButtons[i].setImageResource(diceIdsSelected[number])}
+            }
+            history.add(currentHistory)
+        }
+
+        if(history.size >= 5){
+            history.removeAt(0)
+        }
+
+        updateHistory()
+    }
+
+    private fun updateHistory() {
+        var historyString = ""
+        history.forEach{ array -> array.forEach { value -> historyString += "$value," }; historyString = historyString.removeRange(historyString.lastIndex,historyString.lastIndex+1); historyString += "\n" }
         tvHistory.setText(historyString)
     }
 
@@ -133,14 +148,5 @@ class MainActivity : AppCompatActivity() {
         updateHistory()
     }
 
-//    private fun updateDicesWith(p: Pair<Int, Int>) {
-//        imgDice1.setImageResource( diceIds[p.first-1] )
-//        imgDice2.setImageResource( diceIds[p.second-1] )
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        outState.putSerializable("history", history.toTypedArray())
-//    }
 
 }
