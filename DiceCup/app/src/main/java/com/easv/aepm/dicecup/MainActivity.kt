5485c3964e91c1f6fbb93d54074a7c79870f2169
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-02-25 15:42:58 +0100

    State change

--- DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
+++ DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
@@ -2,17 +2,17 @@ package com.easv.aepm.dicecup
 
 
 import android.content.res.Configuration
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.OnTouchListener
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import com.easv.aepm.dicecup.data.HistoryRoll
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -20,46 +20,124 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     val mGenerator = Random()
-    val diceIds = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
-    val diceIdsSelected = arrayOf(R.drawable.dice1s, R.drawable.dice2s, R.drawable.dice3s, R.drawable.dice4s, R.drawable.dice5s, R.drawable.dice6s)
-    val history = mutableListOf<HistoryRoll>()
+    val diceIds = arrayOf(
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6
+    )
+    val diceIdsSelected = arrayOf(
+        R.drawable.dice1s,
+        R.drawable.dice2s,
+        R.drawable.dice3s,
+        R.drawable.dice4s,
+        R.drawable.dice5s,
+        R.drawable.dice6s
+    )
+    var history = mutableListOf<HistoryRoll>()
+    lateinit var latestHistory: HistoryRoll
 
     var maxButtonsLandscape: Int = 3
     var maxButtonsPortrait: Int = 2
-    var diceChanged: Boolean = false;
+    var maxHistoryLandscape: Int = 15
+    var maxHistoryPortrait: Int = 4
 
+    var diceChanged: Boolean = false
+    var userTouch: Boolean = false
 
-    private lateinit var shouldRoll: Array<Boolean>
+    private var shouldRoll: Array<Boolean> = arrayOf()
     private var diceAmount: Int = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectSpinner.setOnTouchListener(OnTouchListener { v, event ->
+            this.userTouch = true
+            false
+        })
         selectSpinner.setOnItemSelectedListener(object : OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
 
-                diceBoard.removeAllViews()
-                diceAmount = position;
-                shouldRoll = Array(diceAmount){i -> true}
-                diceChanged = true;
 
-                if(position != 0){
-                    generateDice(position)
-                    btnRoll.visibility = View.VISIBLE
-                    tvHistory.visibility = View.VISIBLE
-                    btnClear.visibility = View.VISIBLE
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                if(userTouch){
+                    diceBoard.removeAllViews()
+                    diceAmount = position;
+                    shouldRoll = Array(diceAmount) { i -> true }
+                    diceChanged = true;
+
+                    if (position != 0) {
+                        generateDice(position)
+                        btnRoll.visibility = View.VISIBLE
+                        tvHistory.visibility = View.VISIBLE
+                        btnClear.visibility = View.VISIBLE
+                    } else {
+                        btnRoll.visibility = View.INVISIBLE
+                        tvHistory.visibility = View.INVISIBLE
+                        btnClear.visibility = View.INVISIBLE
+                    }
+                    userTouch = false;
                 }
-                else{
-                    btnRoll.visibility = View.INVISIBLE
-                    tvHistory.visibility = View.INVISIBLE
-                    btnClear.visibility = View.INVISIBLE
-                }
+
+
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         })
 
-        //if(savedInstanceState != null){ }
+        if(savedInstanceState != null){
+            val gson = Gson()
+
+            val jsonHistory = savedInstanceState.getString("history")
+            //this.history = gson.fromJson(jsonHistory, mutableListOf<HistoryRoll>()::class.java)
+
+
+
+
+
+
+
+            if(savedInstanceState.containsKey("latestHistory")){
+                Log.d("YESH", "YESH")
+                val latestHistory = savedInstanceState.getString("latestHistory")
+                this.latestHistory = gson.fromJson(latestHistory, HistoryRoll::class.java)
+            }
+
+
+
+            this.diceChanged = savedInstanceState.getBoolean("diceChanged")
+            this.diceAmount = savedInstanceState.getInt("diceAmount")
+
+            val shouldRoll = savedInstanceState.getString("shouldRoll")
+            this.shouldRoll = gson.fromJson(shouldRoll, Array<Boolean>::class.java)
+//
+
+
+            selectSpinner.setSelection(diceAmount)
+            if(diceAmount !== 0){
+                generateDice(diceAmount)
+                loadoptionsRotation()
+                btnRoll.visibility = View.VISIBLE
+                tvHistory.visibility = View.VISIBLE
+                btnClear.visibility = View.VISIBLE
+            }
+
+
+
+
+
+//            updateHistory()
+
+
+        }
     }
 
     fun generateDice(amount: Int){
@@ -82,12 +160,6 @@ class MainActivity : AppCompatActivity() {
 
                 btn.setBackgroundColor(ContextCompat.getColor(this, R.color.diceboard))
 
-                if(this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
-
-
-
-
-                }
             }
 
             diceBoard.addView(horizontalLayout)
@@ -119,7 +191,7 @@ class MainActivity : AppCompatActivity() {
 
         var historyLast: Int = 0
 
-        if(history.size > 0){
+        if(history.size > 0 ){
 
             if(history[history.lastIndex].history.size === diceAmount){
                 historyLast = history[history.lastIndex].history[placement] -1
@@ -129,16 +201,16 @@ class MainActivity : AppCompatActivity() {
 
         shouldRoll[placement] = !shouldRoll[placement]
         if(diceChanged){
-            imageButton.setImageResource(if(!shouldRoll[placement]) diceIdsSelected[0] else diceIds[0])
+            imageButton.setImageResource(if (!shouldRoll[placement]) diceIdsSelected[0] else diceIds[0])
         }
         else{
-            imageButton.setImageResource(if(!shouldRoll[placement]) diceIdsSelected[historyLast] else diceIds[historyLast])
+            imageButton.setImageResource(if (!shouldRoll[placement]) diceIdsSelected[historyLast] else diceIds[historyLast])
         }
     }
 
     fun onClickRoll(view: View) {
 
-        val currentHistory: Array<Int> = Array<Int>(diceAmount){i -> 0}
+        val currentHistory: Array<Int> = Array<Int>(diceAmount){ i -> 0}
         val allButtons: Array<ImageButton> = getAllButtons()
 
         if(!shouldRoll.contains(false)){
@@ -149,6 +221,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             val roll: HistoryRoll = HistoryRoll(Date(), currentHistory)
+            this.latestHistory = roll
             history.add(roll)
         }
 
@@ -159,7 +232,7 @@ class MainActivity : AppCompatActivity() {
                 if(number == -1 && history.size > 0){
 
                     if(history[history.lastIndex].history.size === diceAmount && !diceChanged){
-                        currentHistory[i] = history[history.size-1].history[i]
+                        currentHistory[i] = history[history.size - 1].history[i]
                     }
                     else{currentHistory[i] = 1}
                 }
@@ -170,11 +243,8 @@ class MainActivity : AppCompatActivity() {
                 if(number != -1){allButtons[i].setImageResource(diceIds[number])}
             }
             val roll: HistoryRoll = HistoryRoll(Date(), currentHistory)
+            this.latestHistory = roll
             history.add(roll)
-        }
-
-        if(history.size >= 5){
-            history.removeAt(0)
         }
 
         updateHistory()
@@ -183,24 +253,58 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateHistory() {
         var historyString = ""
-        history.forEach{ historyRoll -> historyRoll.history.forEach { value -> historyString += "$value," }; historyString = historyString.removeRange(historyString.lastIndex,historyString.lastIndex+1); historyString += "\n" }
+        val historySize: Int = if(this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) maxHistoryPortrait else maxHistoryLandscape
+
+        history.takeLast(historySize).forEach{ historyRoll -> historyRoll.history.forEach { value -> historyString += "$value," }; historyString = historyString.removeRange(
+            historyString.lastIndex,
+            historyString.lastIndex + 1
+        ); historyString += "\n" }
         tvHistory.setText(historyString)
     }
 
     fun onClickClear(view: View) {
+        this.shouldRoll.forEach { d ->  Log.d("DDDDD", "${d}")}
         history.clear()
         updateHistory()
+    }
+
+    fun loadoptionsRotation(){
+
+        val allButtons: Array<ImageButton> = getAllButtons()
+
+        if (this::latestHistory.isInitialized){
+            for(i in allButtons.indices) {
+                allButtons[i].setImageResource(if (!shouldRoll[i]) diceIdsSelected[latestHistory.history[i]-1] else diceIds[latestHistory.history[i]-1])
+            }
+        }
+        else{
+            for(i in allButtons.indices) {
+                allButtons[i].setImageResource(if (!shouldRoll[i]) diceIdsSelected[0] else diceIds[0])
+            }
+        }
+
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
+        val gson = Gson()
+        val jsonHistory = gson.toJson(this.history)
 
-        //Gemme history og should roll
-        //Loade board og sætte terninger til samme billede som sidste history. Hvis
+        val shouldRoll = gson.toJson(this.shouldRoll)
 
+        outState.putString("history", jsonHistory)
+        outState.putString("shouldRoll", shouldRoll)
+        outState.putBoolean("diceChanged", diceChanged)
+        outState.putInt("diceAmount", diceAmount)
+
+        if (this::latestHistory.isInitialized) {
+
+            val jsonLatestHistory = gson.toJson(this.latestHistory)
+            outState.putString("latestHistory", jsonLatestHistory)
+        }
 
     }
-
 
 }
