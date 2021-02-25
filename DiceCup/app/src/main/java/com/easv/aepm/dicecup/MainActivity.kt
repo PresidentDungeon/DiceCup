228ea087aff4bccaef8969e0b62f05f747cb1ac1
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-02-25 10:19:37 +0100

    HistoryDate added

--- DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
+++ DiceCup/app/src/main/java/com/easv/aepm/dicecup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.easv.aepm.dicecup
 
+import android.app.Activity
 import android.graphics.Color
 import android.media.Image
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import androidx.core.view.setPadding
+import com.easv.aepm.dicecup.data.HistoryRoll
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.ceil
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     val mGenerator = Random()
     val diceIds = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
     val diceIdsSelected = arrayOf(R.drawable.dice1s, R.drawable.dice2s, R.drawable.dice3s, R.drawable.dice4s, R.drawable.dice5s, R.drawable.dice6s)
-    val history = mutableListOf<Array<Int>>()
+    val history = mutableListOf<HistoryRoll>()
 
     private lateinit var shouldRoll: Array<Boolean>
     private var diceAmount: Int = 0
@@ -99,7 +101,7 @@ class MainActivity : AppCompatActivity() {
 
         val imageButton: ImageButton = view as ImageButton
         val placement:Int = getIndexOfChild(imageButton)
-        val historyLast: Int = if(history.size > 0) history[history.size-1][placement] - 1 else 0
+        val historyLast: Int = if(history.size > 0) history[history.size-1].history[placement] - 1 else 0
         shouldRoll[placement] = !shouldRoll[placement]
         imageButton.setImageResource(if(!shouldRoll[placement]) diceIdsSelected[historyLast] else diceIds[historyLast])
     }
@@ -115,19 +117,22 @@ class MainActivity : AppCompatActivity() {
                 currentHistory[i] = number + 1
                 allButtons[i].setImageResource(diceIds[number])
             }
-            history.add(currentHistory)
+
+            val roll: HistoryRoll = HistoryRoll(Date(), currentHistory)
+            history.add(roll)
         }
 
         else{
             for(i in allButtons.indices) {
                 val number:Int = if(shouldRoll[i] === false) mGenerator.nextInt(6) else -1
-                if(number == -1 && history.size > 0){currentHistory[i] = history[history.size-1][i]}
+                if(number == -1 && history.size > 0){currentHistory[i] = history[history.size-1].history[i]}
                 else if(number == -1){currentHistory[i] = 0}
                 else{currentHistory[i] = number + 1}
 
                 if(number != -1){allButtons[i].setImageResource(diceIdsSelected[number])}
             }
-            history.add(currentHistory)
+            val roll: HistoryRoll = HistoryRoll(Date(), currentHistory)
+            history.add(roll)
         }
 
         if(history.size >= 5){
@@ -139,13 +144,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateHistory() {
         var historyString = ""
-        history.forEach{ array -> array.forEach { value -> historyString += "$value," }; historyString = historyString.removeRange(historyString.lastIndex,historyString.lastIndex+1); historyString += "\n" }
+        history.forEach{ historyRoll -> historyRoll.history.forEach { value -> historyString += "$value," }; historyString = historyString.removeRange(historyString.lastIndex,historyString.lastIndex+1); historyString += "\n" }
         tvHistory.setText(historyString)
     }
 
     fun onClickClear(view: View) {
         history.clear()
         updateHistory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+
+        //Gemme history og antal knapper valgt.
+        //Loade board og sætte terninger til samme billede som sidste history. Hvis
+
+
     }
 
 
---
+++ DiceCup/app/src/main/java/com/easv/aepm/dicecup/data/HistoryRoll.kt
@@ -0,0 +1,5 @@
+package com.easv.aepm.dicecup.data
+
+import java.util.*
+
+class HistoryRoll (var rollDate: Date, var history: Array<Int>){}
